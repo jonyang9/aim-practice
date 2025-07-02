500137817590c7726b4cda0b81d5e34014e9745f
Jonathan Yang <yangjonathan9@gmail.com>
2025-07-02 12:07:15 -0700

    more html and styling

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 
+// DOM elements
 const header = document.querySelector("header");
 const footer = document.querySelector("footer");
 const controlPanel = document.querySelector(".control-panel");
 const scores = document.querySelector(".scores");
 const colorSelect = document.querySelector(".color-select");
 const timer = document.querySelector(".timer");
+const controlPanelBackground = document.querySelector(".control-panel-bg");
+
+// Game variables
+let score;
+let difficulty;
+// Scores
+
+
 
 // document.addEventListener("DOMContentLoaded", async () => {
 //     await new Promise(resolve => setTimeout(resolve, 5000));
@@ -26,7 +35,7 @@ const playButton = document.querySelector(".play-button");
 playButton.addEventListener("click", async (event) => {
     header.style.display = "none";
     footer.style.display = "none";
-    controlPanel.style.display = "none";
+    controlPanelBackground.style.display = "none";
     scores.style.display = "none";
     colorSelect.style.display = "none";
     let countdown = 3;
