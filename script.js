1be1c74e58e6e93cf21fbd92ae15699e4c7fad9a
Jonathan Yang <yangjonathan9@gmail.com>
2025-07-02 00:55:45 -0700

    styling and js countdown

---
+++ script.js
@@ -0,0 +1,44 @@
+
+const header = document.querySelector("header");
+const footer = document.querySelector("footer");
+const controlPanel = document.querySelector(".control-panel");
+const scores = document.querySelector(".scores");
+const colorSelect = document.querySelector(".color-select");
+const timer = document.querySelector(".timer");
+
+// document.addEventListener("DOMContentLoaded", async () => {
+//     await new Promise(resolve => setTimeout(resolve, 5000));
+//     header.style.display = "none";
+//     footer.style.display = "none";
+//     controlPanel.style.display = "none";
+//     scores.style.display = "none";
+//     colorSelect.style.display = "none";
+//     let countdown = 3;
+//     let intervalId = setInterval(() => {
+//         timer.textContent = countdown.toString();
+//         countdown--;
+//         if (countdown === 0) 
+//             clearInterval(intervalId);
+//     }, 1000);
+// });
+
+const playButton = document.querySelector(".play-button");
+playButton.addEventListener("click", async (event) => {
+    header.style.display = "none";
+    footer.style.display = "none";
+    controlPanel.style.display = "none";
+    scores.style.display = "none";
+    colorSelect.style.display = "none";
+    let countdown = 3;
+    while (countdown >= 0) {
+        timer.textContent = countdown.toString();
+        await delay(1000);
+        countdown--;
+    }
+    
+});
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
